ae8e4979f2abd35abf4a1057fb9b87386de2f465
Ling <nlinh10082007@gmail.com>
2026-03-30 16:59:42 +0700

    2

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+class User{
+    private String fullname;
+    private Double balance;
+    public User(String fullname, Double balance){
+        this.fullname = fullname;
+        this.balance = balance;
+    }
+    public String getFullname(){
+        return fullname;
+    }
+    public Double getBalance(){
+        return balance;
+    }
+
+}
+public class Main {
+}
--- src/main/java/com/example/btl_nhom8/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.btl_nhom8;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
