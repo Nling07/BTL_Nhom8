1ecab8d8759ebf7d98efe96ac51df6eb92bdc8cc
Vanh-VietAnh <25023143@vnu.edu.vn>
2026-05-15 17:45:31 +0700

    Update UserDAO.java

--- src/main/java/com/btl/n8/Connection/UserDAO.java
+++ src/main/java/com/btl/n8/Connection/UserDAO.java
@@ -3,6 +3,8 @@ package com.btl.n8.Connection;
 import com.btl.n8.Model.enums.Role;
 import com.btl.n8.Model.entity.User;
 
+import java.util.List;
+
 public interface UserDAO {
     boolean insert(User user);
     boolean upgradeToSeller(int userId);
@@ -10,4 +12,8 @@ public interface UserDAO {
     boolean setRoleById(int userId, Role role);
     User findByAccount(String account);
     User findById(int id);
+
+    // Bổ sung cho AdminService
+    List<User> findAll();          // Lấy toàn bộ user
+    boolean deleteById(int id);    // Xóa user theo id
 }
